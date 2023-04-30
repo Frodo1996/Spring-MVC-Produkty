a424c92365fd7bc53c6630bf7b102c73f5ac578d
Krzysiek Przystarz <frodson35@gmail.com>
2023-04-30 10:57:55 +0200

    poczatek zadania

---
+++ src/main/java/com/example/zadanie_produkty/ZadanieProduktyApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_produkty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieProduktyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieProduktyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie_produkty/ZadanieProduktyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_produkty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieProduktyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
